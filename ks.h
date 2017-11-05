7f5a80a8bcf9d152c8d084ce66b0f427b20d8b0b
dnbaker <dnb@cs.jhu.edu>
2017-11-04 20:14:03 -0400

    Remove redundant ks.h

--- ks.h
+++ ks.h
@@ -371,15 +371,6 @@ void split(char *s, int delimiter, size_t l, std::vector<T, Alloc> &offsets)
 
 }
 
-inline KString sprintf(const char *fmt, ...) {
-    KString ret;
-    va_list ap;
-    va_start(ap, fmt);
-    ret.vsprintf(fmt, ap);
-    va_end(ap);
-    return ret;
-}
-
 template<typename T=std::size_t, typename=std::enable_if_t<std::is_arithmetic<T>::value>>
 inline void split(char *s, int delimiter, std::vector<T> &offsets) {
     split(s, delimiter, std::strlen(s), offsets);
